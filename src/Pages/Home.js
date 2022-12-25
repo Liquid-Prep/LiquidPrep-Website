eab9f348648e68ad9fa2c3f84d0e8d706abf1984
m4nik1 <maniksoomro@hotmail.com>
2022-12-25 14:50:51 -0500

    First part done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,31 @@
 import React from "react";
 
-import "../css/Homepage.css"
+import { Text, Box, Image, Stack } from "@chakra-ui/react";
+import "../common/images/logo.png"
 
 function HomePage() {
     return (
         <div>
-            <h2 className="topFarmerText">
-                Bringing water 
-                advice to all farmers.
-            </h2>
+            <Stack direction="row">
+                <Box align="left" m={35}>
+                    <Text fontSize='3xl'>
+                        Bringing water <br />
+                        advice to all farmers.
+                    </Text>
+                    <br />
+                    <Text>
+                        Liquid Prep is an open-source project that originates <br />
+                        from the winning team's idea of the Internal IBM Call for <br />
+                        Code challenge in 2019. Our team is dedicated to <br />
+                        helping farmers in developing countries making the <br />
+                        right decisions regarding watering their crops.<br />
+                    </Text>
+                </Box>
+                <Image 
+                    src={require("../common/images/logo.png")}
+                />
+            </Stack>
+
         </div>
     )
 }
