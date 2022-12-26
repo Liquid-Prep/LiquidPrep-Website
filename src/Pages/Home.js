3d7708bff7ca430958753c62e5ebe515d1016358
Charitarth Chugh <charitarth.chughg@gmail.com>
2022-12-26 14:47:28 -0500

    More Home page changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,33 +1,39 @@
 import React from "react";
 
-import { Text, Box, Image, Stack } from "@chakra-ui/react";
-import "../common/images/logo.png"
+import { Text, Box, Image, Stack, Button } from "@chakra-ui/react";
 
 function HomePage() {
-    return (
-        <div>
-            <Stack direction="row">
-                <Box align="left" m={35}>
-                    <Text fontSize='3xl'>
-                        Bringing water <br />
-                        advice to all farmers.
-                    </Text>
-                    <br />
-                    <Text>
-                        Liquid Prep is an open-source project that originates <br />
-                        from the winning team's idea of the Internal IBM Call for <br />
-                        Code challenge in 2019. Our team is dedicated to <br />
-                        helping farmers in developing countries making the <br />
-                        right decisions regarding watering their crops.<br />
-                    </Text>
-                </Box>
-                <Image 
-                    src={require("../common/images/logo.png")}
-                />
-            </Stack>
-
-        </div>
-    )
+  return (
+    <div>
+      <Stack direction="row" align="center" paddingLeft={"6rem"}>
+        <Box align="left" m={35} paddingRight={"5rem"}>
+          <Text fontSize="3xl" align={"left"}>
+            Bringing water <br />
+            advice to all farmers.
+          </Text>
+          <br />
+          <Text align={"left"} paddingBottom={"1rem"}>
+            Liquid Prep is an open-source project that originates <br />
+            from the winning team's idea of the Internal IBM Call for <br />
+            Code challenge in 2019. Our team is dedicated to <br />
+            helping farmers in developing countries making the <br />
+            right decisions regarding watering their crops.
+            <br />
+          </Text>
+          <Button
+            borderRadius={"100px"}
+            background={"#fffffff"}
+            borderColor={"black"}
+            padding={"1rem"}
+            variant={"outline"}
+          >
+            Get Involved
+          </Button>
+        </Box>
+        <Image src={require("../assets/logo.png")} />
+      </Stack>
+    </div>
+  );
 }
 
 export default HomePage;
