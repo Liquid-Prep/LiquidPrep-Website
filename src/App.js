85ec9d5c563acb0f35cb9f3870ed6a94fd643ab0
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2023-01-02 20:37:31 -0500

    format with prettier

--- src/App.js
+++ src/App.js
@@ -7,22 +7,26 @@ import {
 } from "react-router-dom";
 
 import HomePage from "./Pages/Home";
-import Navbar from "./common/navbar"
-
+import Navbar from "./common/navbar";
 
 function App() {
-    return (
-        <div style={{  textAlign: "center", backgroundImage: "linear-gradient(to right, #a9c6e4, #ccdbf0)"
-        , backgroundSize: "fill" }}>
-            <head>
-                <title>LiquidPrep</title>
-            </head>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        backgroundImage: "linear-gradient(to right, #a9c6e4, #ccdbf0)",
+        backgroundSize: "fill",
+      }}
+    >
+      <head>
+        <title>LiquidPrep</title>
+      </head>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
